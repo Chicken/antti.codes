1c0372896221de26eb9b59b96c6ca6bcaefbcd0e
Chicken <antti@antti.codes>
2020-10-02 22:33:45 +0200

    page links

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
-let oof;
-function script() {
-    current = "start"
-    backbutton = Array.from(document.getElementsByClassName("back"))[0]
-    oof = false
-    Array.from(document.getElementsByTagName("button")).forEach(b=>{
-        b.addEventListener("click", () => {
-	    if(oof) new Audio("oof.mp3").play()
-	})
+// variables
+let current = "start";
+let backbutton = Array.from(document.getElementsByClassName("back"))[0];
+let oof = false;
+
+// oof easter egg (turned on from console)
+Array.from(document.getElementsByTagName("button")).forEach(b => {
+    b.addEventListener("click", () => {
+        if(oof) new Audio("oof.mp3").play();
     })
-    Array.from(document.getElementsByClassName("page")).forEach(b=>{
-        b.addEventListener("click", () => {
-            setTimeout(()=>{
-                backbutton.style.display = "block";
-                Array.from(document.getElementsByClassName(b.innerText.toLowerCase())).forEach(c=>{
-                    c.style.display = "block";
-                })
-                Array.from(document.getElementsByClassName(current.toLowerCase())).forEach(c=>{
-                    c.style.display = "none";
-                })
-                current = b.innerText
-            },200)
-        })
+})
+
+// page buttons
+Array.from(document.getElementsByClassName("page")).forEach(b => {
+    b.addEventListener("click", () => {
+        setTimeout(() => {
+            changePage(b.innerText.toLowerCase());
+        }, 200)
     })
-    backbutton.addEventListener("click", ()=>{
-        backbutton.style.display = "none"
-        Array.from(document.getElementsByClassName("start")).forEach(c=>{
-            c.style.display = "block";
-        })
-        Array.from(document.getElementsByClassName(current.toLowerCase())).forEach(c=>{
-            c.style.display = "none";
-        })
-        current = "start"
+})
+
+// backbutton functionality
+backbutton.addEventListener("click", () => {
+    changePage("start");
+})
+
+// site links
+let fragment = window.location.hash.substr(1);
+if(fragment) changePage(fragment);
+
+// page change function
+function changePage(page) {
+    let pages = Array.from(document.getElementsByClassName(page));
+    if(pages.length == 0) {
+        window.location.replace("https://antti.codes/");
+        return;
+    };
+    if (page == "start") {
+        window.location.replace("https://antti.codes/");
+        backbutton.style.display = "none";
+    } else {
+        window.location.replace("https://antti.codes/#" + page);
+        backbutton.style.display = "block";
+    }
+    pages.forEach(c => {
+        c.style.display = "block";
     })
+    Array.from(document.getElementsByClassName(current.toLowerCase())).forEach(c => {
+        c.style.display = "none";
+    })
+    current = page;
 }
